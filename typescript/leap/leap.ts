2f16b9879c488f0e45e14142cd3d4f411165b175
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-11-16 15:39:15 -0500

    Completed leap year TS

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,5 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+function isLeapYear(year: number) {
+  return !(year % 400) || (!(year % 4) && year % 100) ? true : false;
 }
 
-export default isLeapYear
+export default isLeapYear;
