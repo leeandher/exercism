a1d261bc621c959dd02af47d4a5a636e0c8b1d89
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-11-17 15:15:22 -0500

    refactored cipher into one function

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -1,24 +1,49 @@
 class SimpleCipher {
-    //Fields
-    key: string;
+  //Fields
+  key: string;
 
-    //Constructor
-    constructor() {
-        const keyArr: string[];
-        const keyBase = 'abcdefghijklmnopqrstuvwxyz';
-        
+  //Constructor
+  constructor(key: string = "") {
+    //Initalize the key
+    this.key = key;
 
-        this.key = 
+    //Create the key if not provided
+    const keyBase = "abcdefghijklmnopqrstuvwxyz";
+    if (!key.length) {
+      for (let i = 0; i <= 100; i++) {
+        this.key += keyBase[Math.floor(Math.random() * keyBase.length)];
+      }
     }
-    //Functions
+  }
 
-    encode( /* Parameters go here */ ) {
-        // Your code here
-    }
+  //Functions
+  shift(text: string, encode: boolean) {
+    let shifted = "";
+    for (let i = 0; i < text.length; i++) {
+      //Initialize the shift paramters
+      const keyShift = this.key.charCodeAt(i) - 97;
+      let shiftedCharCode: number;
 
-    decode( /* Parameters go here */ ) {
-        // Your code here
+      //Determine the shift direction (encoding or decoding)
+      if (encode) {
+        shiftedCharCode = text.charCodeAt(i) + keyShift;
+        if (shiftedCharCode > 122) shiftedCharCode -= 26;
+      } else {
+        shiftedCharCode = text.charCodeAt(i) - keyShift;
+        if (shiftedCharCode < 97) shiftedCharCode += 26;
+      }
+      shifted += String.fromCharCode(shiftedCharCode);
     }
+    return shifted;
+  }
+
+  encode(text: string) {
+    return this.shift(text, true);
+  }
+
+  decode(text: string) {
+    return this.shift(text, false);
+  }
 }
 
-export default SimpleCipher
+export default SimpleCipher;
